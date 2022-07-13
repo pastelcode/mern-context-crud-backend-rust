f2c53107230094cac230f5fffb38c575f416b8e8
Pastel <pastel.dev.gt@gmail.com>
2022-07-13 13:19:45 -0600

    Manage MongoDB repository on rocket's state

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+mod models;
+mod repository;
+
+use repository::mongodb_repository::MongoRepository;
 use rocket::{build, launch, routes, Build, Rocket};
 
 #[launch]
 async fn rocket() -> Rocket<Build> {
-    build().mount("/", routes![])
+    let db = MongoRepository::init().await;
+    build().manage(db).mount("/", routes![])
 }
