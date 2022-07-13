ac6479e7cd10a630552ee2cecf3d47f491b40586
Pastel <pastel.dev.gt@gmail.com>
2022-07-13 16:32:15 -0600

    Create method to fetch a user from db by id

--- src/repository/posts_repository.rs
+++ src/repository/posts_repository.rs
@@ -1,5 +1,5 @@
 use crate::{models::post_model::Post, repository::mongodb_repository::MongoRepository};
-use mongodb::bson::extjson::de::Error;
+use mongodb::bson::{doc, extjson::de::Error, oid::ObjectId};
 use rocket::futures::StreamExt;
 
 impl MongoRepository {
@@ -13,5 +13,17 @@ impl MongoRepository {
             .collect::<Vec<Post>>()
             .await;
         Ok(posts)
+    }
+
+    pub async fn get_post(&self, post_id: &String) -> Result<Post, Error> {
+        let parsed_post_id = ObjectId::parse_str(post_id).unwrap();
+        let filter = doc! {"_id": parsed_post_id};
+        let post = self
+            .posts_collection
+            .find_one(filter, None)
+            .await
+            .unwrap()
+            .unwrap();
+        Ok(post)
     }
 }
