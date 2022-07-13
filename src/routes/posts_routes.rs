fcab6e223afbbad60d36601dc2fa363ff1bba83e
Pastel <pastel.dev.gt@gmail.com>
2022-07-13 16:32:31 -0600

    Create route to get user

--- src/routes/posts_routes.rs
+++ src/routes/posts_routes.rs
@@ -10,3 +10,15 @@ pub async fn get_all_posts(db: &State<MongoRepository>) -> Result<Json<Vec<Post>
         Err(_) => Err(Status::InternalServerError),
     }
 }
+
+#[get("/<post_id>")]
+pub async fn get_post(db: &State<MongoRepository>, post_id: String) -> Result<Json<Post>, Status> {
+    if post_id.is_empty() {
+        return Err(Status::BadRequest);
+    }
+    let result = db.get_post(&post_id).await;
+    match result {
+        Ok(post) => Ok(Json(post)),
+        Err(_) => Err(Status::InternalServerError),
+    }
+}
