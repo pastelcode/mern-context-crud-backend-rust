571cf8d5324bd2508e4df8d3e4ceefcb7b90afae
Pastel <pastel.dev.gt@gmail.com>
2022-07-13 16:32:45 -0600

    Mount route to get a post

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ mod routes;
 
 use repository::mongodb_repository::MongoRepository;
 use rocket::{build, launch, routes, Build, Rocket};
-use routes::posts_routes::get_all_posts;
+use routes::posts_routes::{get_all_posts, get_post};
 
 #[launch]
 async fn rocket() -> Rocket<Build> {
     let db = MongoRepository::init().await;
-    build().manage(db).mount("/posts", routes![get_all_posts])
+    build()
+        .manage(db)
+        .mount("/posts", routes![get_all_posts, get_post])
 }
