0cd8ed8bab08e1847c6da0f8522c72178fe445d7
Pastel <pastel.dev.gt@gmail.com>
2022-07-13 14:33:11 -0600

    Mount `get_all_posts` on `/posts` base

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod models;
 mod repository;
+mod routes;
 
 use repository::mongodb_repository::MongoRepository;
 use rocket::{build, launch, routes, Build, Rocket};
+use routes::posts_routes::get_all_posts;
 
 #[launch]
 async fn rocket() -> Rocket<Build> {
     let db = MongoRepository::init().await;
-    build().manage(db).mount("/", routes![])
+    build().manage(db).mount("/posts", routes![get_all_posts])
 }
