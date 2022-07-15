81235a14b45e1b2aef29ab2e0d145a94100cfa7f
Pastel <pastel.dev.gt@gmail.com>
2022-07-15 13:40:29 -0600

    Create `tmp` directory to persist images

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ mod upload_image_to_cloudinary;
 
 use initialize_env_variables::initialize_env_variables;
 use repository::mongodb_repository::MongoRepository;
-use rocket::{build, launch, routes, Build, Rocket};
+use rocket::{build, launch, routes, tokio::fs::create_dir_all, Build, Rocket};
 use routes::posts_routes::{create_post, get_all_posts, get_post};
 
 #[launch]
 async fn rocket() -> Rocket<Build> {
     initialize_env_variables();
+    // Create `tmp` folder to storage temporary images to upload to Cloudinary
+    create_dir_all("./tmp/").await.unwrap();
     let db = MongoRepository::init().await;
     build()
         .manage(db)
